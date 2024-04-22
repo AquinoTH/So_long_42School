856517703486287178debf042525600aee7e3748
AquinoTH <thiago4912@hotmail.com>
2024-04-22 11:52:27 -0300

    updating repo - project in progress

--- src/so_long.h
+++ src/so_long.h
@@ -6,16 +6,33 @@
 /*   By: taquino- <taquino-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 19:06:39 by taquino-          #+#    #+#             */
-/*   Updated: 2024/04/11 20:06:21 by taquino-         ###   ########.fr       */
+/*   Updated: 2024/04/21 13:16:48 by taquino-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "../Libs/mlx/mlx.h"
+# include "../mlx/mlx.h"
+# include "../libft/libft.h"
+# include <X11/keysym.h>
+# include < X11/X.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <stdlib.h>
 # include <stdio.h>
 # include <fcntl.h>
+# include <unistd.h>
+
+
+typedef struct s_image
+{
+	void	
+}	t_image;
+
+
+
+
 
 typedef struct s_data
 {
